a325733402437cc25bb4a6574889aed122a010ee
anas <hameed.anas@gmail.com>
2024-08-24 16:58:55 +0500

    Implement the paypal integration

--- src/App.js
+++ src/App.js
@@ -1,76 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-
-const stripePromise = loadStripe(
-  "pk_test_51OoFBuAH8GwybafKNXJCRm5pW3z3L4dnGHWwYeIS4rlbfy52c3psZ6Qv1FY55ibOVTr9I5iW4acy4Tr2XsUNAgt600MODEZnYL"
-);
-
-const SavePayment = () => {
-  const elements = useElements();
-  const stripe = useStripe();
-
-  const handleSavePaymentMethod = async (event) => {
-    event.preventDefault();
-    const card = elements.getElement(CardElement);
-    console.log("element", card);
-    // create the payment method and link it to the customer id on stripe
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: "card",
-      card: card,
-    });
-
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Payment method saved:", paymentMethod);
-      console.log("Payment method ID:", paymentMethod.id);
-      // Here you would typically send the setupIntent.client_secret to your server to save the payment method ID
-    }
-  };
-
-  return (
-    <form onSubmit={handleSavePaymentMethod}>
-      <CardElement />
-      <button>Submit</button>
-    </form>
-  );
-};
+import React from "react";
+import Paypal from "./paypal";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-
-  const handleChargeCard = async () => {
-    setLoading(true);
-    const stripe = await stripePromise;
-    const { error, paymentIntent } = await stripe.confirmCardPayment(
-      "client_secret_for_payment_intent", // You need to generate this payment intent on your server
-      {
-        payment_method: "pm_card_visa", // Example payment method ID
-      }
-    );
-    if (error) {
-      console.log(error.message);
-    } else {
-      console.log("Payment successful:", paymentIntent.id);
-    }
-    setLoading(false);
-  };
-
-  const options = {
-    // passing the client secret obtained from the server
-    clientSecret:
-      "seti_1PgAxXAH8GwybafK77odGeHu_secret_QXFSHIpAbmQF2AwzTP5iFresVLJVXzJ",
-  };
-
-  return loading ? (
-    <h1>loading</h1>
-  ) : (
+  return (
     <div>
-      <Elements stripe={stripePromise} options={options}>
-        <SavePayment />
-      </Elements>
+      <Paypal />
     </div>
   );
 }
---
+++ src/paypal.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+// Renders errors or successfull transactions on the screen.
+function Message({ content }) {
+  return <p>{content}</p>;
+}
+
+function Paypal() {
+  const initialOptions = {
+    "client-id":
+      "client_id",
+    "disable-funding": "paylater,card",
+    currency: "USD",
+    "data-page-type": "product-details",
+    components: "buttons",
+    "data-sdk-integration-source": "developer-studio",
+  };
+
+  const [message, setMessage] = useState("");
+  const onApprove = async (data, actions) => {
+    try {
+      const url = `http://localhost:8000/api/v1/payment/paypal/orders/${data.orderID}/capture`;
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const orderData = await response.json();
+      // Three cases to handle:
+      //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+      //   (2) Other non-recoverable errors -> Show a failure message
+      //   (3) Successful transaction -> Show confirmation or thank you message
+
+      const errorDetail = orderData?.details?.[0];
+      if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
+        // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+        // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
+        return actions.restart();
+      } else if (errorDetail) {
+        // (2) Other non-recoverable errors -> Show a failure message
+        throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
+      } else {
+        // (3) Successful transaction -> Show confirmation or thank you message
+        // Or go to another URL:  actions.redirect('thank_you.html');
+        const transaction = orderData.purchase_units[0].payments.captures[0];
+        setMessage(
+          `Transaction ${transaction.status}: ${transaction.id}. See console for all available details`
+        );
+        console.log(
+          "Capture result",
+          orderData,
+          JSON.stringify(orderData, null, 2)
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      setMessage(`Sorry, your transaction could not be processed...${error}`);
+    }
+  };
+
+  const onCreate = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:8000/api/v1/payment/paypal/checkout/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: "YOUR_PRODUCT_ID",
+            quantity: "YOUR_PRODUCT_QUANTITY",
+            hours: 2,
+            tutorId: "66aa7094c0b910bf07aca695",
+          }),
+        }
+      );
+
+      const orderData = await response.json();
+      if (orderData.id) {
+        return orderData.id;
+      } else {
+        const errorDetail = orderData?.details?.[0];
+        const errorMessage = errorDetail
+          ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
+          : JSON.stringify(orderData);
+
+        throw new Error(errorMessage);
+      }
+    } catch (error) {
+      console.error(error);
+      setMessage(`Could not initiate PayPal Checkout...${error}`);
+    }
+  };
+
+  return (
+    <div className="App">
+      <PayPalScriptProvider options={initialOptions}>
+        <PayPalButtons
+          style={{
+            shape: "rect",
+            layout: "vertical",
+            color: "gold",
+            label: "paypal",
+          }}
+          createOrder={onCreate}
+          onApprove={onApprove}
+        />
+      </PayPalScriptProvider>
+      <Message content={message} />
+      <p>Hello I am Anas</p>
+    </div>
+  );
+}
+
+export default Paypal;
